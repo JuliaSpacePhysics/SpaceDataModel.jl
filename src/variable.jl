03e065ebb2da9bfa6f59837bb9946a122674ac06
Zijin Zhang <58776897+Beforerr@users.noreply.github.com>
2025-04-14 23:22:47 -0700

    feat: remove unitful deps and add `units` interface (#2)

--- src/variable.jl
+++ src/variable.jl
@@ -10,12 +10,13 @@ time(v) = v.time
 
 function Base.show(io::IO, var::T) where {T<:AbstractDataVariable}
     ismissing(var) && return
-    print(io, "$T(")
     print_name(io, var)
-    print(io, ", Time Range: ", time(var)[1], " to ", time(var)[end])
-    print(io, ", Units: ", unit(var))
+    print(io, " [")
+    print(io, "Time Range: ", time(var)[1], " to ", time(var)[end])
+    u = units(var)
+    isnothing(u) || print(io, ", Units: ", u)
     print(io, ", Size: ", size(var))
-    print(io, ")")
+    print(io, "]")
 end
 
 # Add Base.show methods for pretty printing
@@ -25,7 +26,8 @@ function Base.show(io::IO, m::MIME"text/plain", var::T) where {T<:AbstractDataVa
     print_name(io, var)
     println(io)
     println(io, "  Time Range: ", time(var)[1], " to ", time(var)[end])
-    println(io, "  Units: ", unit(var))
+    u = units(var)
+    isnothing(u) || println(io, "  Units: ", u)
     println(io, "  Size: ", size(var))
     println(io, "  Memory Usage: ", Base.format_bytes(Base.summarysize(var)))
     if (m = meta(var)) !== nothing
