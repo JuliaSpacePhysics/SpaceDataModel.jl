8ead585ec48c0a198a0e56906e9829cad6daad6c
Beforerr <zzj956959688@gmail.com>
2025-04-23 10:18:39 -0700

    fix: edge case when no time

--- src/variable.jl
+++ src/variable.jl
@@ -8,13 +8,17 @@ Optional:
 * `times(v)`: the timestamps of the variable
 * `units(v)`: the units of the variable
 * `meta(v)`: the metadata of the variable
+* `name(v)`: the name of the variable
 """
 abstract type AbstractDataVariable{T,N} <: AbstractArray{T,N} end
 
 # https://docs.julialang.org/en/v1/manual/interfaces/#man-interface-array
 Base.parent(var::AbstractDataVariable) = var.data
-Base.size(var::AbstractDataVariable) = size(parent(var))
 Base.iterate(A::AbstractDataVariable, args...) = iterate(parent(A), args...)
+for f in (:size, :Array)
+    @eval Base.$f(var::AbstractDataVariable) = $f(parent(var))
+end
+
 for f in (:getindex,)
     @eval Base.$f(var::AbstractDataVariable, I::Vararg{Int}) = $f(parent(var), I...)
 end
@@ -23,14 +27,17 @@ times(v) = v.times
 tmin(v) = minimum(times(v))
 tmax(v) = maximum(times(v))
 
+_timerange_str(times) = "Time Range: $(minimum(times)) to $(maximum(times))"
+
 function Base.show(io::IO, var::T) where {T<:AbstractDataVariable}
     ismissing(var) && return
     print_name(io, var)
     print(io, " [")
-    print(io, "Time Range: ", tmin(var), " to ", tmax(var))
+    time = times(var)
+    isnothing(time) || print(io, _timerange_str(time), ",")
     u = units(var)
-    isnothing(u) || print(io, ", Units: ", u)
-    print(io, ", Size: ", size(var))
+    isnothing(u) || print(io, " Units: ", u, ",")
+    print(io, " Size: ", size(var))
     print(io, "]")
 end
 
@@ -40,7 +47,8 @@ function Base.show(io::IO, m::MIME"text/plain", var::T) where {T<:AbstractDataVa
     print(io, "$T: ")
     print_name(io, var)
     println(io)
-    println(io, "  Time Range: ", tmin(var), " to ", tmax(var))
+    time = times(var)
+    isnothing(time) || println(io, "  ", _timerange_str(time))
     u = units(var)
     isnothing(u) || println(io, "  Units: ", u)
     println(io, "  Size: ", size(var))
