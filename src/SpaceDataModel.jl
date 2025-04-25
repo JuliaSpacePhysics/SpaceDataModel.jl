26a75638c0e114a4fcabca686ac9b6e8a94e3fb0
Beforerr <zzj956959688@gmail.com>
2025-04-25 14:54:49 -0700

    fix: code order

--- src/SpaceDataModel.jl
+++ src/SpaceDataModel.jl
@@ -9,12 +9,6 @@ export Project, Instrument, DataSet, LDataSet, Product
 export Event
 export abbr
 
-# Interface
-name(v) = _getfield(v, :name)
-meta(v) = _getfield(v, (:meta, :metadata))
-units(v) = @get(v, "units", nothing)
-times(v) = _getfield(v, (:times, :time))
-
 include("utils.jl")
 include("types.jl")
 include("dataset.jl")
@@ -23,5 +17,11 @@ include("variable.jl")
 include("catalog.jl")
 include("methods.jl")
 include("timerange.jl")
+
+# Interface
+name(v) = _getfield(v, :name)
+meta(v) = _getfield(v, (:meta, :metadata))
+units(v) = @get(v, "units", nothing)
+times(v) = _getfield(v, (:times, :time))
 
 end
